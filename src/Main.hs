9e1fe70ed258460f6ad21051c9a1530a80a6570b
zelca <zelcadev@gmail.com>
2016-05-03 23:25:47 +0200

    Add simple Main example

---
+++ src/Main.hs
@@ -0,0 +1,7 @@
+module Main where
+
+main = do putStrLn "What is 2 + 2?"
+          x <- readLn
+          if x == 4
+              then putStrLn "You're right!"
+              else putStrLn "You're wrong!"
